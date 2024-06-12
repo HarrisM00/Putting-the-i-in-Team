43577b904a0fae271bf1f57f5a20601969becba2
HarrisM00 <Harrismahmood014@gmail.com>
2024-06-12 14:06:11 -0400

    Batman added

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('heroes-container');
 
+  // Data array for superheroes
   const heroes = [
     {
+      name: 'Batman',
+      position: 'Detective',
+      skills: ['Martial Arts', 'Stealth', 'Intelligence'],
+      strengths: ['Strategic Thinking', 'Combat Skills'],
+      weaknesses: ['No Superpowers'],
+      biography: 'A member of the Justice League and an iconic hero in DC Comics. His real identity is Bruce Wayne, a wealthy businessman. He dedicated his life to fighting crime after the murder of his parents.',
+      img: 'batman.jpg',
+      class: ''
+    },
+  
